ce90f3d16b4a15168de6935936a1fa54d4571b66
Anders Abel <anders.abel@sustainsys.com>
2025-10-16 11:07:18 +0200

    Add Projects

---
+++ Web1/Pages/Secure.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace Web1.Pages;
+
+[Authorize]
+public class SecureModel : PageModel
+{
+    public SessionData Session { get; set; } = default!;
+
+    public async Task OnGet()
+    {
+        // retrieve current session
+        var authenticateResult = await HttpContext.AuthenticateAsync();
+
+        var userClaims = authenticateResult.Principal!.Claims;
+        var metadata = authenticateResult.Properties!.Items;
+
+        Session = new SessionData(userClaims, metadata!);
+    }
+
+    public record SessionData(
+        IEnumerable<Claim> Claims,
+        IDictionary<string, string> Metadata);
+}
