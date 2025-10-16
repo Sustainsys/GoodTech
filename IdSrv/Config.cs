75f6dd119cb42c7a0d8bf6077d27a27a10c067e9
Anders Abel <anders.abel@sustainsys.com>
2025-10-16 11:29:41 +0200

    Get client credentials token

--- IdSrv/Config.cs
+++ IdSrv/Config.cs
@@ -14,8 +14,8 @@ public static class Config
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope("scope1"),
-            new ApiScope("scope2"),
+            new ApiScope("api1"),
+            new ApiScope("api2"),
         };
 
     public static IEnumerable<Client> Clients =>
@@ -24,29 +24,44 @@ public static class Config
             // m2m client credentials flow client
             new Client
             {
-                ClientId = "m2m.client",
+                ClientId = "m2m",
                 ClientName = "Client Credentials Client",
 
                 AllowedGrantTypes = GrantTypes.ClientCredentials,
                 ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
-                AllowedScopes = { "scope1" }
+                AllowedScopes = { "api1", "api2" }
             },
 
             // interactive client using code flow + pkce
             new Client
             {
-                ClientId = "interactive",
+                ClientId = "web1",
                 ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
                 AllowedGrantTypes = GrantTypes.Code,
 
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                RedirectUris = { "https://localhost:5001/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:5001/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-oidc" },
 
                 AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
+                AllowedScopes = { "openid", "profile", "api1", "api2" }
+            },
+
+            new Client
+            {
+                ClientId = "web2",
+                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+
+                RedirectUris = { "https://localhost:5001/signin-oidc" },
+                FrontChannelLogoutUri = "https://localhost:5001/signout-oidc",
+                PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "api1" }
             },
         };
 }
--- Machine/Program.cs
+++ Machine/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Duende.IdentityModel.Client;
+using System.Text.Json;
+using static System.Console;
+
+WriteLine("Requesting Access token...");
+WriteLine();
+
+HttpClient tokenClient = new();
+
+ClientCredentialsTokenRequest tokenRequest = new()
+{
+    RequestUri = new("https://localhost:5000/connect/token"),
+    ClientId = "m2m",
+    ClientSecret = "511536EF-F270-4058-80CA-1C89C192F69A",
+    Scope = "api1 api2"
+};
+
+var tokenResponse = await tokenClient.RequestClientCredentialsTokenAsync(tokenRequest);
+
+WriteLine($"Token Endpoint Status Code: {(int)tokenResponse.HttpStatusCode}");
+WriteLine("Token Response");
+
+var jDoc = JsonDocument.Parse(tokenResponse.Raw!);
+
+var pretty = JsonSerializer.Serialize(
+    jDoc, new JsonSerializerOptions() { WriteIndented = true });
+
+WriteLine(pretty);
